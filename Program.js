a1640e63c6e7f43679c12f80de4966e766ff31ea
GDSIEB01 <griffin.siebert@stu.jefferson.kyschools.us>
2023-04-17 07:59:08 -0400

    Created Initial Commit and Initial Files.

---
+++ Program.js
@@ -0,0 +1,11 @@
+const QuestionList = [
+    "[\"Hip\",\"Hip!\"]"
+]
+const AnswerList = [
+    "Hip Hip Array"
+]
+
+function executeOrder66() {
+    var index = Math.floor(Math.random() * QuestionList.length);
+    
+}
