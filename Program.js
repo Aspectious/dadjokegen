898fd6e9934a64d874a5e352b58b35e0f21c4614
GDSIEB01 <griffin.siebert@stu.jefferson.kyschools.us>
2023-04-17 08:28:36 -0400

    Howdy :>

--- Program.js
+++ Program.js
@@ -1,11 +1,52 @@
 const QuestionList = [
-    "[\"Hip\",\"Hip!\"]"
+    "[\"Hip\",\"Hip!\"]",
+    "{!False}",
+    "Dad, I'm Hungry!",
+    "127.0.0.1",
+    "Why don’t bachelors like Git?",
+    "What do you call a skinny ghost?",
+    "What’s the second movie about a database engineer called?",
+    "Karl Marx would really not like Java",
+    "I was having a hard time understanding source control…"
 ]
 const AnswerList = [
-    "Hip Hip Array"
+    "Hip Hip Array",
+    "It's funny because it's True.",
+    "Hi hungry, I'm Dad!",
+    "There's no place like home.",
+    "Because they are afraid to commit.",
+    "BOOLEAN.",
+    "The SQL ",
+    "He was really more a classless sorta guy.",
+    "but i’m starting to git it "
 ]
 
 function executeOrder66() {
     var index = Math.floor(Math.random() * QuestionList.length);
-    
+    console.clear();
+    console.log(QuestionList[index] + "\n" + AnswerList[index]);
+    document.getElementById("sus").remove();
+    var card = document.createElement("div")
+    card.id = "sus"
+    card.onclick = function() {
+        card.childNodes[0].hidden = !card.childNodes[0].hidden
+        card.childNodes[1].hidden = !card.childNodes[1].hidden
+    }
+    card.classList.add("card")
+
+    var front = document.createElement("span");
+    front.hidden = false;
+    front.innerHTML = QuestionList[index];
+    front.classList.add("nosel","text")
+
+
+    var back = document.createElement("span");
+    back.hidden = true;
+    back.innerHTML = AnswerList[index]
+    back.classList.add("nosel","text")
+
+
+
+    card.append(front, back);
+    document.getElementById("maindiv").append(card);
 }
